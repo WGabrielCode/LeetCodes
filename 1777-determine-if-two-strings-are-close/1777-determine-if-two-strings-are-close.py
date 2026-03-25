3d9b365b8997446819c14243f99039410a91b8c1
Werminski_Gabriel <gwerminski@student.agh.edu.pl>
2026-03-25 16:38:44 +0100

    1777-DETERMINE-IF-TWO-STRINGS-ARE-CLOSE Stats: Time: 81 ms (73.92%), Space: 20.8 MB (16.74%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
@@ -4,25 +4,14 @@ class Solution:
         if len( word1 ) != len( word2 ) :
             return False 
         
-        s1 , s2 = set( word1 ) , set( word2 )
+        from collections import defaultdict, Counter
         
-        if s1 != s2  :
+        cnt1 = Counter( word1 ) 
+        cnt2 = Counter( word2 ) 
+    
+        if cnt1.keys() != cnt2.keys() :
             return False 
-        
-        cnt1 = {}
-        for char1 in word1 :
-            if not char1 in cnt1 :
-                cnt1[char1] = 1
-            else :
-                cnt1[char1] += 1
 
-        cnt2 = {}
-        for char2 in word2 :
-            if not char2 in cnt2 :
-                cnt2[char2] = 1
-            else :
-                cnt2[char2] += 1
-        
         if sorted( cnt1.values() ) == sorted( cnt2.values() ) :
             return  True # sorted( cnt1.values() ) , sorted( cnt2.values() )
 
