b69e2eb352095d5831ac5511f966fd9ac24a5519
Gabriel <gwerminski@student.agh.edu.pl>
2026-06-20 13:45:47 +0200

    1116-MAXIMUM-LEVEL-SUM-OF-A-BINARY-TREE Stats: Time: 9 ms (96.13%), Space: 22.8 MB (80.73%) - LeetHub

--- 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.py
+++ 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.py
@@ -11,32 +11,35 @@ class Solution:
 
         from collections import deque
         
-        last_lvl = 1
-        last_sum = 0
+
+
+        curr_lvl = 0
 
         max_sum = -float("inf")
         max_lvl = -1
 
         q = deque()
-        q.append( (root,1) )
+        q.append( root )
 
         while q :
-            (head, curr_lvl) = q.popleft()
-            if last_lvl < curr_lvl:
-                if last_sum > max_sum :
-                    max_sum = last_sum 
-                    max_lvl = last_lvl
+            curr_lvl += 1
+            lvl_size = len( q )
+            lvl_sum = 0
 
-                last_lvl = curr_lvl
-                last_sum = head.val
-            else :
-                last_sum += head.val
 
-            if not (head.left is None) :
-                q.append( (head.left,curr_lvl+1 ) )
-            if not (head.right is None ):
-                q.append( (head.right,curr_lvl+1 ) )
-        if max_sum < last_sum :
-            max_lvl = last_lvl
+            for _ in range( lvl_size ) :
+                head = q.popleft()
+                
+                lvl_sum += head.val
+
+                if head.left is not None :
+                    q.append(head.left)
+                if head.right is not None :
+                    q.append(head.right)
+                
+            if lvl_sum > max_sum :
+                max_sum = lvl_sum
+                max_lvl = curr_lvl
+        
         return max_lvl
         
